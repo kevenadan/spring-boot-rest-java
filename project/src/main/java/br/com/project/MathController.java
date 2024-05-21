17cfbbb1618b358bc0272db0fb49d21c98d2a71c
kevenadan <kevenadanb@gmail.com>
2024-05-21 08:17:04 -0300

    Implemente as Demais Operações Matemáticas

--- project/src/main/java/br/com/project/MathController.java
+++ project/src/main/java/br/com/project/MathController.java
@@ -23,7 +23,77 @@ public class MathController {
 		
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);
 	}
+	
+	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}", 
+			method = RequestMethod.GET)
+	public Double subtract(
+			@PathVariable(value= "numberOne") String numberOne,
+			@PathVariable(value= "numberTwo") String numberTwo
+			)throws Exception {
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Por favor informe um valor numérico");
+		}
+		
+		return convertToDouble(numberOne) - convertToDouble(numberTwo);
+	}
+	
+	@RequestMapping(value= "/multiplication/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double multiplication(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+			) throws Exception {
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Por favor informe um valor numérico");
+		}
+		
+		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+	}
+	
+	@RequestMapping(value = "/division/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double division(
+			@PathVariable(value= "numberOne") String numberOne,
+			@PathVariable(value= "numberTwo") String numberTwo
+			) throws Exception {
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Por favor informe um valor numérico");
+		}
+		
+		return convertToDouble(numberOne) / convertToDouble(numberTwo);
+	}
+	
+	@RequestMapping(value = "/average/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double average(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value= "numberTwo") String numberTwo
+			) throws Exception {
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Por favor informe um valor numérico");
+		}
+		
+		return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+	}
 
+	// implementar o endpoint da operação Raiz quadrada
+	
+	@RequestMapping(value = "/squareroot/{number}",
+			method = RequestMethod.GET)
+	public Double squareroot(
+			@PathVariable(value = "number") String number
+			) throws Exception {
+		
+		if(!isNumeric(number)) {
+			throw new UnsupportedMathOperationException("Por favor informe um valor numérico");
+		}
+		
+		return Math.sqrt(convertToDouble(number));
+	}
+	
 	private Double convertToDouble(String strNumber) {
 		if(strNumber == null) return 0D;
 		//BR 10,25 US 10.25
@@ -31,10 +101,12 @@ public class MathController {
 		if(isNumeric(number)) return Double.parseDouble(number);
 		return 0D;
 	}
-;
+
 	private boolean isNumeric(String strNumber) {
 		if(strNumber == null) return false;
 		String number = strNumber.replaceAll(",", ".");
 		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
+	
+
 }
